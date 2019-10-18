3fdea252970bed24b35f2c4890882c60342f6f4a
Ammar Ghazali <ammar_ghazali@yahoo.de>
2019-10-18 20:00:09 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hoho there!</p>
+        <p>Kihkih there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
